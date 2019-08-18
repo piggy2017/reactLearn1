0d4563bbde275ae192c6ea147482087903602066
sunlin <1164700321@qq.com>
2019-08-18 20:08:14 +0800

    first update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// JSX ===javascript and xml
+ReactDOM.render(<App/>,document.getElementById("root"));
+// 自定义组件必须以大写字母开头  <App/>
